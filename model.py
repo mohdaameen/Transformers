abf71b8f6fc627fe7e63eecd9d2343d77024f5e8
mohdaameen <md.aameen2710@gmail.com>
2024-10-28 13:58:01 +0530

    ADDED TWO MORE CLASSES

--- model.py
+++ model.py
@@ -13,6 +13,74 @@ class InputEmbeddings(nn.Module):
     def forward(self,x):
         return self.embeddings(x) * math.sqrt(self.d_model)
     
+class  PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seqlen = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        #create a matrix of shape(seq_len, d_model)
+
+        pe = torch.zeros(seq_len, d_model)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0,d_model,2).float()*(-math.log(10000.0)/d_model))
+
+        #apply the sin to the even position
+        pe[:,0::2] = torch.sin(position*div_term)
+
+        #apply the cos to the odd position
+        pe[:,1::2] = torch.cos(position*div_term)
+
+        print(pe)
+
+        pe = pe.unsqueeze(0)
+
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:,:x.shape[1],:]).requires_grad_(False)
+
+        return self.dropout(x)
+    
+class LayerNormalization(nn.Module):
+
+    def __init__(self, eps: float = 10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) #Multiplied
+        self.bias = nn.Parameter(torch.zeros(1)) #Added
+    
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim = True)
+        std = x.std(dim= -1, keepdim = True)
+        return self.alpha*(x-mean)/(std + self.eps) + self.bias
+    
+class FeedForwardBlock(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff) #W1 and b1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)#w2 and b2
+
+
+    def forward(self, x):
+        #(Batch, Seq_Len, D_model)-->(BAtch, Seq_Len, d_ff)--.(Batch, Seq_Len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
